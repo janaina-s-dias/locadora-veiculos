5dc9f77170c249a87ac8ee866183ee80317aa20f
Janaina Dias <janainasdias95@gmail.com>
2018-03-28 11:57:39 -0300

    Fibonacci

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sequeência Fibonnaci");
+
+            int qtd = 15;
+
+            int a, b, c;
+            a = 1;
+            b = 1;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 3; i <= qtd; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(c);
+            }
+
+        }
+    }
+}
