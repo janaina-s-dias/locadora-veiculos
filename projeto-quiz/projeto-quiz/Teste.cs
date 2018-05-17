08627fe2bd64c3358ff61c4808cd8b12e5097069
Janaina Dias <janainasdias95@gmail.com>
2018-05-17 09:35:16 -0300

    commit

---
+++ projeto-quiz/projeto-quiz/Teste.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projeto_quiz
+{
+    public class Teste
+    {
+        public String nome { get; set; }
+        public int pontos { get; set; }
+        public DateTime dataRealizado { get; set; }
+
+
+        public Dictionary<int, string> method = new Dictionary<int, string>();
+
+
+
+    }
+}
